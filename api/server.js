37640fde680db917b0b4cef96c25e2f335673f5a
Numan71417 <89739573+Numan71417@users.noreply.github.com>
2023-09-03 10:49:06 +0530

    Added model, db

--- api/server.js
+++ api/server.js
@@ -1,14 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { mongoose } from 'mongoose';
 
+dotenv.config();
 const PORT = process.env.PORT | 8000
 const app = express();
 
 
 app.get('/', (req, res)=>{
-    console.log(req);
     return res.status(234).send("Welcome")
 })
 
+const connectDb = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+
+        console.log(`server running on: ${mongoose.connection.host}`);
+    } catch (e) {
+        console.log(" error: ", e );
+    }
+}
+connectDb();
 
 app.listen(PORT , ()=>{
     console.log(`App is working on PORT ${PORT}`)
